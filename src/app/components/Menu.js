72fe3ce00ccbadbb60efab773fe4b9144f14aa36
longnq264 <longhb2642002@gmail.com>
2024-03-19 02:13:40 +0700

    new message

---
+++ src/app/components/Menu.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+const Menu = () => {
+  return (
+    <nav className="nav-bar-menu">
+      <ul>
+        <li>
+          <Link href="/product">Products & Solutions</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+        <li>
+          <Link href="/resources">Resources</Link>
+        </li>
+        <li>
+          <Link href="/product">Contact Us</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
---
+++ src/app/components/Slider.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Slider from "react-slick";
+// import Carousel from "react-bootstrap/Carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SimpleSlider = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default SimpleSlider;
